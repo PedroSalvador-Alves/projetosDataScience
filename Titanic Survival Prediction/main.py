73f322c17770d34c6aebb3d9fef5598d0608d402
Pedro Salvador <p187044@dac.unicamp.br>
2022-01-11 14:20:28 -0300

    Studying

---
+++ Titanic Survival Prediction/main.py
@@ -0,0 +1,17 @@
+###############################################
+# Author: Pedro Igor Salvador Alves
+# Project: Titanic Survival Prediction
+# Start Date: 30/12/2021
+# Type: Data Science - Machine Learning 
+###############################################
+
+#importing libraries for data analysis and visualization
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#reading data
+train = pd.read_csv("data/train.csv")
+test = pd.read_csv("data/test.csv")
+
